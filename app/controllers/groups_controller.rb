8ac1a94e2180822f3819e382723fd9b0834b995b
kagi-opendoor222 <opendoor222@gmail.com>
2018-08-07 16:13:41 +0900

    edit GroupsController for group-create-system

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,16 @@
 class GroupsController < ApplicationController
   def new
-
+    @group = Group.new
   end
   def create
-
+    @group = Group.new(create_params)
+    if @group.save
+      @group.users << User.where(id: params[:group][:user_ids])
+      flash[:notice] = "グループ作成しました。"
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   def show
 
@@ -13,5 +20,9 @@ class GroupsController < ApplicationController
   end
   def update
 
+  end
+  private
+  def create_params
+    params.require(:group).permit(:name)
   end
 end
