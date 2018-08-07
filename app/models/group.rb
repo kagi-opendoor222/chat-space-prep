639a2416541afdfef3030de6b862f0704116fed1
kagi-opendoor222 <opendoor222@gmail.com>
2018-08-07 16:14:14 +0900

    set validation in model-Group

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,6 @@ class Group < ApplicationRecord
   #association
   has_many :group_users
   has_many :users, through: :group_users
+  #validation
+  validates :name, presence: true, uniqueness: true, length: {minimum: 3}
 end
