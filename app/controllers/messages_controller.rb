61d59955c176141657ab329fad0198e02b9c004d
kagi-opendoor222 <opendoor222@gmail.com>
2018-08-08 23:45:44 +0900

    edit message#create for post-message-system

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,16 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
   def create
-
+    group =  Group.find(5)
+    message = group.messages.new(message_params.merge(user_id: current_user.id))
+    if message.save
+      redirect_to root_path
+    else
+      redirect_to root_path, alert: "メッセージ送信に失敗しました。"
+    end
+  end
+  private
+  def message_params
+    params.require(:message).permit(:content, :image)
   end
 end
