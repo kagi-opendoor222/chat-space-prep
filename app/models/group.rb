d2b4cbbb4352693eecb06801cd2df5f3844da3d6
kagi-opendoor222 <opendoor222@gmail.com>
2018-08-08 17:16:53 +0900

    set association between message and user, group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ApplicationRecord
   #association
   has_many :group_users
   has_many :users, through: :group_users
+  has_many :messages
   #validation
   validates :name, presence: true, uniqueness: true, length: {minimum: 3}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   #assosiation
   has_many :group_users
   has_many :groups, through: :group_users
+  has_many :messages
 end
