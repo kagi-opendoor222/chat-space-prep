b3690b7ceb4ec6cfc9081b1d7ae1901a89670704
kagi-opendoor222 <opendoor222@gmail.com>
2018-08-08 17:33:38 +0900

    set validation on message-model

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,5 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
   #validation
-  validates :content, presence: true, unless: image?
+  validates :content, presence: true, unless: :image?
 end
