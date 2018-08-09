ac1822bf28c755c37960c29471895ea3819ac553
kagi-opendoor222 <opendoor222@gmail.com>
2018-08-09 16:33:02 +0900

    define set_group and replace previous code

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,10 @@
 class MessagesController < ApplicationController
+  before_action :set_group, only: [:index, :create]
   def index
-    @group = Group.find(params[:group_id])
     @message = Message.new
   end
   def create
-    group =  Group.find(5)
-    message = group.messages.new(message_params.merge(user_id: current_user.id))
+    message = @group.messages.new(message_params.merge(user_id: current_user.id))
     if message.save
       redirect_to root_path
     else
@@ -15,5 +14,8 @@ class MessagesController < ApplicationController
   private
   def message_params
     params.require(:message).permit(:content, :image)
+  end
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
