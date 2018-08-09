175557311df2a6029c5d2716dcf087b6bc24ee73
kagi-opendoor222 <opendoor222@gmail.com>
2018-08-09 19:10:04 +0900

    define display_latest_message and replace previous code

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,14 @@ class Group < ApplicationRecord
   has_many :messages
   #validation
   validates :name, presence: true, uniqueness: true, length: {minimum: 3}
+
+  def display_latest_message
+    if self.messages.last && self.messages.last.content.present?
+      self.messages.last.content
+    elsif self.messages.last && self.messages.last.image
+      "画像が表示されています。"
+    else
+      "メッセージが投稿されていません。"
+    end
+  end
 end
