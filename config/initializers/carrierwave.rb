e8bcd99c51ddc611fd6d72b7320b36fe862f11fa
kagi-opendoor222 <opendoor222@gmail.com>
2018-11-01 03:12:55 +0900

    12

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'kagi-upload-test'
-  config.asset_host = 's3-ap-northeast-1.amazonaws.com/kagi-upload-test'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kagi-upload-test'
 end
